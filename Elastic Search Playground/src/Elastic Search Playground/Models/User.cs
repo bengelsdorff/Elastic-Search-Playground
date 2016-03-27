670a1efa2d0a94a6cdfc877453d6ff32e5983044
Antonio Bengelsdorff <antonio.bengelsdorff@gmail.com>
2016-03-27 23:48:42 +0100

    Clean default stuff + Add nest package

---
+++ Elastic Search Playground/src/Elastic Search Playground/Models/User.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Elastic_Search_Playground.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Age { get; set; }
+
+    }
+}
